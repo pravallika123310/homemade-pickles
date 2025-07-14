072465b1ef0abccbe43c9b2adc1c93a51e52faea
pravallika123310 <pravallika.challa004@gmail.com>
2025-07-14 22:20:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,5 @@ def feedback():
     return render_template('feedback.html')
 
 # ---------- Run ----------
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
