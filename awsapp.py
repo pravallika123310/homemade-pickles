63fb1936837e66962d2a398402633379694b2399
pravallika123310 <pravallika.challa004@gmail.com>
2025-07-16 18:28:20 +0530

    Update awsapp.py

--- awsapp.py
+++ awsapp.py
@@ -271,4 +271,4 @@ def feedback():
 
 # ----------- Run App -----------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
